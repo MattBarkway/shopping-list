86ffd43aaeefa88dbcf1b96a7bfe70a81649f7b0
MattBarkway <contact@mattbarkway.dev>
2023-05-19 21:24:55 +0100

    bits

--- src/endpoints/shopping.rs
+++ src/endpoints/shopping.rs
@@ -1,7 +1,20 @@
-use actix_web::{get, web, Responder};
+use actix_web::{get, put, web, Responder};
+use serde::Deserialize;
 
 
 #[get("/shopping/{id}")]
 pub async fn get_shopping_list(id: web::Path<usize>) -> impl Responder {
     format!("Here is shopping list {id}!")
+}
+
+
+#[derive(Deserialize)]
+pub struct ShoppingItem {
+    name: String,
+    quantity: usize,
+}
+
+#[put("/shopping/{id}")]
+pub async fn add_shopping_list_item(id: web::Path<usize>, item: web::Json<ShoppingItem>) -> impl Responder {
+    format!("You have added {} of item: {} to list: {id}!", item.quantity, item.name)
 }
