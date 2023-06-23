c650860fb7955be5f2cb2269cfdd5ab004ce383d
Matt Barkway <matt.barkway@ovo.com>
2023-06-23 12:15:37 +0100

    go with fastAPI instead

--- src/database/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod interface;
-pub mod schema;
-pub mod surreal;
--- src/endpoints/payloads.rs
+++
@@ -1,7 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Deserialize, Serialize)]
-pub struct CreateShoppingList {
-    pub name: String,
-    pub user: String,
-}
