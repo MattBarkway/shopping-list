5dd9d36d295f09ca42f81bef7fe0198b49755051
MattBarkway <contact@mattbarkway.dev>
2023-07-14 15:33:09 +0100

    tf fixes

--- deploy/ecr/main.tf
+++ deploy/ecr/main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+  }
+}
+
+# Provider configuration
+provider "aws" {
+  region = "eu-west-2"
+}
+
 resource "aws_ecr_repository" "shopping_list_ecr" {
   name = var.ecr_repository_name
 }
