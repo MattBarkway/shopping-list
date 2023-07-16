4774830da30e37878cf3b0871fd8324d021d819d
MattBarkway <contact@mattbarkway.dev>
2023-07-16 14:28:56 +0100

    switch to ssh

--- deploy/service/main.tf
+++ deploy/service/main.tf
@@ -39,6 +39,7 @@ resource "aws_instance" "frontend_instance" {
   instance_type = "t2.micro"
   associate_public_ip_address = true
   subnet_id     = aws_subnet.subnet_a.id
+  key_name = "shopping-list"
 
   vpc_security_group_ids = [aws_security_group.frontend_sg.id]
   tags = {
@@ -51,6 +52,7 @@ resource "aws_instance" "backend_instance" {
   instance_type = "t2.micro"
   associate_public_ip_address = true
   subnet_id     = aws_subnet.subnet_b.id
+  key_name = "shopping-list"
 
   vpc_security_group_ids = [aws_security_group.backend_sg.id]
   tags = {
@@ -62,6 +64,13 @@ resource "aws_security_group" "frontend_sg" {
   name        = "frontend-security-group"
   description = "Security group for the frontend instance"
   vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   ingress {
     from_port   = 80
@@ -82,6 +91,13 @@ resource "aws_security_group" "backend_sg" {
   name        = "backend-security-group"
   description = "Security group for the backend instance"
   vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   ingress {
     from_port   = 8000
