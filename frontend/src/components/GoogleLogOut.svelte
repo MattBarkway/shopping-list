9a115b542e9904e4d892bcd3328c466e55ef2743
Matt Barkway <matt.barkway@ovo.com>
2024-01-26 15:18:09 +0000

    google auth

---
+++ frontend/src/components/GoogleLogOut.svelte
@@ -0,0 +1,10 @@
+<script>
+  window.signOut = () => {
+    const auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(() => {
+      // User is now signed out
+    });
+  }
+</script>
+
+<a onclick="signOut">Sign out</a>
---
+++ frontend/src/components/GoogleSignIn.svelte
@@ -0,0 +1,26 @@
+<script>
+  window.onSignIn = (googleUser) => {
+     const profile = googleUser.getBasicProfile();
+     console.log('ID: ' + profile.getId());
+     console.log('Name: ' + profile.getName());
+     console.log('Image URL: ' + profile.getImageUrl());
+     console.log('Email: ' + profile.getEmail());
+  };
+</script>
+
+<svelte:head>
+  <script src="https://apis.google.com/js/platform.js?onload=onLoadCallback" async defer></script>
+</svelte:head>
+
+<div class="g-signin2" data-longtitle="true" data-onsuccess="onSignIn" />
+
+<!-- Whack this somewhere? -->
+<!--<script>-->
+<!--  let userLoggedIn = false;-->
+<!--  window.onLoadCallback = () => {-->
+<!--    userLoggedIn = gapi.auth2.getAuthInstance().isSignedIn.get();-->
+<!--    if (userLoggedIn) {  -->
+<!--      // Handle login-->
+<!--    }-->
+<!--  }-->
+<!--</script>-->
